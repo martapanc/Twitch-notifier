55bf5bfd92fa7cf1d7794ce395fb6038c05de922
Marta Pancaldi <marta.panc@gmail.com>
2020-07-07 21:29:42 +0100

    Set alert frequency based on UTC time of day

--- config.py
+++ config.py
@@ -3,5 +3,33 @@ config = {
     'webhook-url': 'https://hooks.slack.com/services/{}',
     'twitch-live-url': 'https://api.twitch.tv/helix/streams?user_id={}',
     'twitch-token-url': 'https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type=client_credentials',
-    'twitch-user-follows-url': 'https://api.twitch.tv/helix/users/follows?from_id={}&first=100'
+    'twitch-user-follows-url': 'https://api.twitch.tv/helix/users/follows?from_id={}&first=100',
+    'twitch-channel-url': 'https://www.twitch.tv/{}'
+}
+
+schedule = {
+    0: 60,
+    1: 60,
+    2: 60,
+    3: 60,
+    4: 60,
+    5: 60,
+    6: 60,
+    7: 60,
+    8: 30,
+    9: 30,
+    10: 30,
+    11: 30,
+    12: 10,
+    13: 10,
+    14: 10,
+    15: 10,
+    16: 5,
+    17: 5,
+    18: 5,
+    19: 5,
+    20: 5,
+    21: 5,
+    22: 5,
+    23: 5
 }
--- cronjob.py
+++ cronjob.py
@@ -6,16 +6,20 @@ from main import cron_job
 
 from dotenv import load_dotenv
 from pathlib import Path
+from datetime import datetime
+from config import schedule
 
 # Setup functions to read from .env file
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-seconds_elapsed = 30
 
-cron_job(seconds_elapsed)
+# Determine the frequency of the updates based on the time of day
+minutes_elapsed = schedule[datetime.utcnow().hour]
+
+cron_job(minutes_elapsed)
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(lambda: cron_job(seconds_elapsed), "interval", seconds=seconds_elapsed)
+scheduler.add_job(lambda: cron_job(minutes_elapsed), "interval", minutes=minutes_elapsed)
 
 scheduler.start()
