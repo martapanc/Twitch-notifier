5d0f39e3e823a5da987269aa728dffa954d9485d
Marta Pancaldi <marta.panc@gmail.com>
2020-07-10 00:24:44 +0100

    Add test live_queue class

---
+++ live_queue.py
@@ -0,0 +1,22 @@
+
+live_queue = []
+
+
+def get_queue():
+    return live_queue
+
+
+def add_to_queue(live_channel):
+    live_queue.append(live_channel)
+
+
+def remove_from_queue(channel):
+    if channel in live_queue:
+        live_queue.remove(channel)
+
+
+def is_in_queue(channel):
+    for live in live_queue:
+        if live == channel:
+            return True
+    return False
